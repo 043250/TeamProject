7f2ad8504587556758d35a666fefe4251481453f
박상진 <043250@naver.com>
2026-03-20 18:19:32 +0900

    충돌로 인한 파훼

--- Source/TeamProject/MyActor.cpp
+++ Source/TeamProject/MyActor.cpp
@@ -13,21 +13,15 @@ void AMyActor::BeginPlay()
 {
 	Super::BeginPlay();
 
-<<<<<<< HEAD
 	//UE_LOG(LogTemp, Warning, TEXT("AMyActor has begun play!"));
-=======
 	UE_LOG(LogTemp, Warning, TEXT("AMyActor has begun play!"));
->>>>>>> b6434f2a08aeb3fc076df3fe131f4db542f2ed81
 }
 
 void AMyActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-<<<<<<< HEAD
 	//UE_LOG(LogTemp, Warning, TEXT("AMyActor is ticking!"));
-=======
 	UE_LOG(LogTemp, Warning, TEXT("AMyActor is ticking!"));
->>>>>>> b6434f2a08aeb3fc076df3fe131f4db542f2ed81
 }
 
